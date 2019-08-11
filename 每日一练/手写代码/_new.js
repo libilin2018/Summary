5b3339016fcd6db5e49ce4560d36d4396610791a
libilin2018 <2335225976@qq.com>
2019-08-11 21:41:48 +0800

    add

--- 每日一练/手写代码/_new.js
+++ 每日一练/手写代码/_new.js
@@ -13,3 +13,14 @@ function _new1 (fn, ...args) {
     const ret = fn.apply(obj, args)
     return Object.prototype.toString.call(ret) == '[object Object]' ? ret : obj
 }
+
+function _new2 () {
+    let Con = [].shift.call(arguments);
+    let obj = Object.create(Con.prototype);
+    let ret = Con.apply(obj, arguments);
+    return Object.prototype.toString.call(ret) === '[object object]' ? ret : obj;
+}
+function Foo (name) {
+    this.name = name
+}
+console.log(_new(Foo, 'billy'), _new1(Foo, 'billy'), _new2(Foo, 'billy'))
