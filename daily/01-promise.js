c2fc396b383e0e291b56617776744894d4889105
libilin2018 <2335225976@qq.com>
2019-08-21 16:02:59 +0800

    add

--- daily/01-promise.js
+++ daily/01-promise.js
@@ -6,17 +6,59 @@ const taskA = new Promise(resolve => {
     setTimeout(()=>{
         console.log('filled A', logTime(time));
         resolve('A');
-    }, 1000);
-})
-const taskB = new Promise(resolve => {
-    setTimeout(()=>{
-        console.log('filled B', logTime(time));
-        resolve('B');
     }, 3000);
 })
-
-Promise.all([taskA, taskB]).then((values) => {
+const taskB = new Promise((resolve, reject) => {
     setTimeout(()=>{
-        console.log('filled C', logTime(time));
+        console.log('filled B', logTime(time));
+        reject('B');
     }, 1000);
 })
+
+const taskC = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        console.log('filled C', logTime(time));
+        resolve('C');
+    }, 2000);
+})
+
+/* Promise._race = promises => new Promise((resolve, reject) => {
+    promises.forEach(promise => {
+        promise.then(resolve, reject)
+    })
+}) */
+
+/* 
+Promise.all 错误处理
+  Promise.all()执行多个网络请求，如果有一个被拒绝就立刻调用，不会等待其他promise执行结束
+1. 全部改为串行调用（失去Node并发优势）
+2. 使用 try catch 处理异常，返回resolve
+*/
+
+function promiseAll(promises){
+    return new Promise((resolve, reject) => {
+        if (!promises[Symbol.iterator]) return reject(new TypeError(`${promises} is not iterable(cannot read property Symbol(Symbol.iterator))`))
+        let countNum = 0;
+        let promiseNum = promises.length;
+        let resolvedArr = new Array(promiseNum)
+        for (let i=0; i<promiseNum; i++) {
+            Promise.resolve(promises[i]).then(value => {
+                countNum++
+                resolvedArr[i] = value
+                if (countNum === promiseNum) {
+                    resolve(resolvedArr)
+                }
+            }).catch(reason => {
+                reject(reason)
+            })
+        }
+    })
+}
+promiseAll([taskA, taskB]).then(function(value){
+    console.log('resolve' + value)
+}).catch(value => {
+    console.log('reject' + value)
+})
+
+
+
